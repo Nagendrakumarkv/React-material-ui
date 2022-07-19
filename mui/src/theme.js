227b2a8c773f321e64e4ea6b78ec3e393611ec45
Nagendra Kumar K V <nagendrakumarkv123@gmail.com>
2022-07-19 19:49:55 +0530

    react material ui

---
+++ mui/src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+export const theme=createTheme({
+    palette:{
+        primary:{
+            main:'red',
+            light:'yellow'
+        },
+        secondary:{
+            main:'gray'
+        },
+        otherColor:{
+            main:'black'
+        }
+    }
+})
